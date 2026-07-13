98ed47a6a531a2cda8412597bd3cf9e3c3b583ea
Eline Anjos <elineanjos18@gmail.com>
2026-07-13 13:46:56 -0300

    Adicionei ferramentas Linha, mão livre e quadrado

--- controlador/ferramentaDesenho.py
+++ controlador/ferramentaDesenho.py
@@ -2,13 +2,14 @@ from dataclasses import dataclass
 from abc import ABC, abstractmethod
 
 
-from modelo import MaoLivre
+from modelo.MaoLivre import MaoLivre
 from modelo.borracha import Borracha
 from modelo.retangulo import Retangulo
 from modelo.oval import Oval
 from modelo.circulo import Circulo
 from modelo.borracha import Borracha
-# (Eline adicione os imports seus aq depois: Linha, Quadrado, etc)
+from modelo.Linha import Linha
+from modelo.Quadrado import Quadrado
 
 # Classe Base (O Molde do Professor)
 @dataclass
@@ -177,4 +178,102 @@ class Borracha_Ferramenta(Ferramenta):
     def mouse_solto(self, event):
         if not self.borracha_atual.vazia():
             self.desenho.adiciona_figura(self.borracha_atual)
+        self.desenho.desenha_figuras(self.canvas)
+
+#ferramenta Linha
+@dataclass
+class Linha_Ferramenta(Ferramenta):
+    linha_atual: 'Linha' = None
+
+    def mouse_pressionado(self, event):
+        cor = self.visao.cor_borda_var.get() or "black"
+
+        self.linha_atual = Linha.a_partir_de_pontos(
+            event.x,
+            event.y,
+            event.x,
+            event.y,
+            cor)
+
+    def mouse_arrastado(self, event):
+        self.linha_atual = Linha.a_partir_de_pontos(
+            self.linha_atual.x_inicio,
+            self.linha_atual.y_inicio,
+            event.x,
+            event.y,
+            self.linha_atual.cor)
+
+        self.desenho.desenha_figuras(self.canvas)
+        self.linha_atual.desenha(self.canvas, dash=(4, 2))
+
+    def mouse_solto(self, event):
+        if not self.linha_atual.vazia():
+            self.desenho.adiciona_figura(self.linha_atual)
+        self.desenho.desenha_figuras(self.canvas)     
+
+#ferramenta Mão Livre
+@dataclass
+class MaoLivre_Ferramenta(Ferramenta):
+    mao_livre_atual: 'MaoLivre' = None
+
+    def mouse_pressionado(self, event):
+        cor = self.visao.cor_borda_var.get() or "black"
+
+        self.mao_livre_atual = MaoLivre.a_partir_do_primeiro_ponto(
+            event.x,
+            event.y,
+            cor)
+
+    def mouse_arrastado(self, event):
+        self.mao_livre_atual.adicionar_ponto(
+            event.x,
+            event.y)
+
+        self.desenho.desenha_figuras(self.canvas)
+        self.mao_livre_atual.desenha(self.canvas)
+
+    def mouse_solto(self, event):
+        if not self.mao_livre_atual.vazia():
+            self.desenho.adiciona_figura(self.mao_livre_atual)
+
+        self.mao_livre_atual = None
+
+        self.desenho.desenha_figuras(self.canvas)
+
+#ferramenta Quadrado
+@dataclass
+class Quadrado_Ferramenta(Ferramenta):
+    quadrado_atual: 'Quadrado' = None
+
+    def mouse_pressionado(self, event):
+        cor_borda = self.visao.cor_borda_var.get() or "black"
+        cor_preenchimento = (
+            self.visao.cor_preenchimento_var.get() or "black")
+
+        self.quadrado_atual = Quadrado.a_partir_de_pontos(
+            event.x,
+            event.y,
+            event.x,
+            event.y,
+            cor_borda,
+            cor_preenchimento)
+
+    def mouse_arrastado(self, event):
+        self.quadrado_atual = Quadrado.a_partir_de_pontos(
+            self.quadrado_atual.x_inicio,
+            self.quadrado_atual.y_inicio,
+            event.x,
+            event.y,
+            self.quadrado_atual.cor_borda,
+            self.quadrado_atual.cor_preenchimento)
+
+        self.desenho.desenha_figuras(self.canvas)
+        self.quadrado_atual.desenha(
+            self.canvas,
+            dash=(4, 2))
+
+    def mouse_solto(self, event):
+        if not self.quadrado_atual.vazia():
+            self.desenho.adiciona_figura(
+                self.quadrado_atual)
         self.desenho.desenha_figuras(self.canvas)
